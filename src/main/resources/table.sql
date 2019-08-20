cba5ae9cbc920f37ab1389722ca967b831b576c0
skysean-coding <->
2019-08-20 09:31:28 +0800

    #建表sql#

---
+++ src/main/resources/table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `cipher_text` (
+  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `content` text COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
